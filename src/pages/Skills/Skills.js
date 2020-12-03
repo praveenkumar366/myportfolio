518989af0b0478073eb91eb5cbfd26c9436555d3
praveekumar366 <praveenkimi@yahoo.com>
2020-12-03 20:37:36 +0530

    skills ui

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,38 +1,43 @@
 import React, { Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import skill_data from "./myskills.json";
-import '../Skills/Skills.css'
+import "../Skills/Skills.css";
 
 export default function Skills() {
-//   console.log(skill_data);
+  //   console.log(skill_data);
   return (
     <Fragment>
       <h1>Skills</h1>
       <Container>
-        {skill_data.map((skill_data,index) => {
-          const perc = '%';
-            const styles = {
-              width: skill_data.percent + perc,
+        {skill_data.map((skill_data, index) => {
+          const perc = "%";
+          const styles = {
+            width: skill_data.percent + perc,
           };
           console.log(styles);
           return (
-              <Row key={skill_data.skill}>
-                  {
-                    <Col lg="12">
-                      <div className="progress-wrap">
-                      <h3>{skill_data.skill}</h3>
-                      <div className="progress">
-								 	<div className="progress-bar color-1" role="progressbar" aria-valuenow="95"
-								  	aria-valuemin="0" aria-valuemax="100" style={styles}>
-								    <span>{skill_data.percent}%</span>
-								  	</div>
-								</div>
+            <Row key={skill_data.skill} className="main_row">
+              {
+                <Col lg="12">
+                  <div className="progress-wrap">
+                    <h3 className="headi_skill">{skill_data.skill}</h3>
+                    <div className="progress">
+                      <div
+                        className="progress-bar color-1"
+                        role="progressbar"
+                        aria-valuenow="95"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                        style={styles}
+                      >
+                        <span>{skill_data.percent}%</span>
+                      </div>
                     </div>
-                    </Col>
-                   
-                  }
-                  </Row>
-          )
+                  </div>
+                </Col>
+              }
+            </Row>
+          );
         })}
       </Container>
     </Fragment>
